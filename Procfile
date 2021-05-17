ac72879692ccfacf6b1776f6fcf6c5e36e346710
Ibukun Badmus <b.ibukun@semicolon.africa>
2021-05-17 15:19:37 +0100

    myPractice

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn myNewSite.wsgi --log-file -
---
+++ polls/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from polls.models import Question
+
+
+# Create your views here.
+def index(request):
+    questions = Question.objects.all()
+
+    context = {
+        "questions": questions
+    }
+
+    return render(request, "polls/index.html", context)
